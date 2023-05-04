81288fa044b15808a28b00cfe4303633503beea3
noname <48761044+noname0310@users.noreply.github.com>
2023-05-04 13:21:14 +0900

    lint

--- src/loader/parser/MmdDataDeserializer.ts
+++ src/loader/parser/MmdDataDeserializer.ts
@@ -1,7 +1,7 @@
 type TupleOf<T, N extends number, R extends unknown[]> =
-    R['length'] extends N ? R : TupleOf<T, N, [T, ...R]>;
+    R["length"] extends N ? R : TupleOf<T, N, [T, ...R]>;
 
-type Tuple<T, N extends number> = N extends N 
+type Tuple<T, N extends number> = N extends N
     ? number extends N ? T[] : TupleOf<T, N, []>
     : never;
 
