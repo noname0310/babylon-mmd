a499a4cbc187f5d1adf6a930d929779f2682e6dd
noname <48761044+noname0310@users.noreply.github.com>
2023-05-29 03:13:57 +0900

    Create MmdStandardMaterial.ts

---
+++ src/loader/MmdStandardMaterial.ts
@@ -0,0 +1,24 @@
+import * as BABYLON from "@babylonjs/core";
+
+import { MmdPluginMaterial } from "./MmdPluginMaterial";
+
+export class MmdStandardMaterial extends BABYLON.StandardMaterial {
+    private readonly _pluginMaterial: MmdPluginMaterial;
+
+    public constructor(name: string, scene?: BABYLON.Scene) {
+        super(name, scene);
+        this.backFaceCulling = false;
+        this.specularColor = new BABYLON.Color3(0, 0, 0);
+
+        const pluginMaterial = this._pluginMaterial = new MmdPluginMaterial(this);
+        pluginMaterial.isEnabled = true;
+    }
+
+    public get sphereTexture(): BABYLON.Texture | null {
+        return this._pluginMaterial.sphereTexture;
+    }
+
+    public set sphereTexture(value: BABYLON.Texture | null) {
+        this._pluginMaterial.sphereTexture = value;
+    }
+}
