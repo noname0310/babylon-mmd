97772d02e0ca0efc5aee03947630e02a03ce8ec3
noname <48761044+noname0310@users.noreply.github.com>
2023-06-07 09:13:11 +0900

    disable outline for debug

--- src/runtime/instance/SceneBuilder.ts
+++ src/runtime/instance/SceneBuilder.ts
@@ -19,6 +19,7 @@ import {
     Vector3
 } from "@babylonjs/core";
 
+import type { MmdStandardMaterialBuilder } from "@/loader/MmdStandardMaterialBuilder";
 import { PmxLoader } from "@/loader/PmxLoader";
 
 import type { ISceneBuilder } from "../base/ISceneBuilder";
@@ -26,11 +27,11 @@ import type { ISceneBuilder } from "../base/ISceneBuilder";
 export class SceneBuilder implements ISceneBuilder {
     public build(canvas: HTMLCanvasElement, engine: Engine): Scene {
         const pmxLoader = new PmxLoader();
-        // const materialBuilder = pmxLoader.materialBuilder as MmdStandardMaterialBuilder;
+        const materialBuilder = pmxLoader.materialBuilder as MmdStandardMaterialBuilder;
         // materialBuilder.loadDiffuseTexture = (): void => { /* do nothing */ };
         // materialBuilder.loadSphereTexture = (): void => { /* do nothing */ };
         // materialBuilder.loadToonTexture = (): void => { /* do nothing */ };
-        // materialBuilder.loadOutlineRenderingProperties = (): void => { /* do nothing */ };
+        materialBuilder.loadOutlineRenderingProperties = (): void => { /* do nothing */ };
         SceneLoader.RegisterPlugin(pmxLoader);
 
         const scene = new Scene(engine);
