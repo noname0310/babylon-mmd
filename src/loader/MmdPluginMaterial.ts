53db8dda38d41072fc8dd22512bcd7e7dd6ae60f
noname <48761044+noname0310@users.noreply.github.com>
2023-06-07 10:06:17 +0900

    sdef shader wip

--- src/loader/MmdPluginMaterial.ts
+++ src/loader/MmdPluginMaterial.ts
@@ -175,16 +175,93 @@ export class MmdPluginMaterial extends MaterialPluginBase {
                     attribute vec3 matricesSdefR0;
                     attribute vec3 matricesSdefR1;
 
-                    vec4 rotationMatrixToQuaternion(mat4 matrix) {
-                        
+                    vec4 rotationMatrixToQuaternion(mat3 matrix) {
+                        float trace = matrix[0][0] + matrix[1][1] + matrix[2][2];
+                        float s;
+
+                        if (trace > 0.0) {
+                            s = 0.5 / sqrt(trace + 1.0);
+
+                            return vec4(
+                                (matrix[2][1] - matrix[1][2]) * s,
+                                (matrix[0][2] - matrix[2][0]) * s,
+                                (matrix[1][0] - matrix[0][1]) * s,
+                                0.25 / s
+                            );
+                        } else if (matrix[0][0] > matrix[1][1] && matrix[0][0] > matrix[2][2]) {
+                            s = 2.0 * sqrt(1.0 + matrix[0][0] - matrix[1][1] - matrix[2][2]);
+
+                            return vec4(
+                                0.25 * s,
+                                (matrix[0][1] + matrix[1][0]) / s,
+                                (matrix[0][2] + matrix[2][0]) / s,
+                                (matrix[2][1] - matrix[1][2]) / s
+                            );
+                        } else if (matrix[1][1] > matrix[2][2]) {
+                            s = 2.0 * sqrt(1.0 + matrix[1][1] - matrix[0][0] - matrix[2][2]);
+
+                            return vec4(
+                                (matrix[0][1] + matrix[1][0]) / s,
+                                0.25 * s,
+                                (matrix[1][2] + matrix[2][1]) / s,
+                                (matrix[0][2] - matrix[2][0]) / s
+                            );
+                        } else {
+                            s = 2.0 * sqrt(1.0 + matrix[2][2] - matrix[0][0] - matrix[1][1]);
+
+                            return vec4(
+                                (matrix[0][2] + matrix[2][0]) / s,
+                                (matrix[1][2] + matrix[2][1]) / s,
+                                0.25 * s,
+                                (matrix[1][0] - matrix[0][1]) / s
+                            );
+                        }
+                    }
+
+                    mat3 quaternionToRotationMatrix(vec4 q) {
+                        float xx = q.x * q.x;
+                        float yy = q.y * q.y;
+                        float zz = q.z * q.z;
+                        float xy = q.x * q.y;
+                        float zw = q.z * q.w;
+                        float zx = q.z * q.x;
+                        float yw = q.y * q.w;
+                        float yz = q.y * q.z;
+                        float xw = q.x * q.w;
+
+                        return mat3(
+                            1.0 - 2.0 * (yy + zz), 2.0 * (xy + zw), 2.0 * (zx - yw),
+                            2.0 * (xy - zw), 1.0 - 2.0 * (zz + xx), 2.0 * (yz + xw),
+                            2.0 * (zx + yw), 2.0 * (yz - xw), 1.0 - 2.0 * (yy + xx)
+                        );
+                    }
+
+                    vec4 slerp(vec4 q0, vec4 q1, float t) {
+                        float cosTheta = dot(q0, q1);
+
+                        if (cosTheta < 0.0) {
+                            q1 = -q1;
+                            cosTheta = -cosTheta;
+                        }
+
+                        if (cosTheta > 0.9995) {
+                            return normalize(mix(q0, q1, t));
+                        }
+
+                        float theta = acos(cosTheta);
+                        float sinTheta = sin(theta);
+
+                        float w0 = sin((1.0 - t) * theta) / sinTheta;
+                        float w1 = sin(t * theta) / sinTheta;
+
+                        return q0 * w0 + q1 * w1;
                     }
                 #endif
             `;
 
             codes[`!${this.escapeRegExp("finalWorld=finalWorld*influence;")}`] = /* glsl */`
                 #if defined(SDEF)
-                    mat4 sdefInflunce;
-
+                {
                     float weight0 = matricesWeights[0];
                     float weight1 = matricesWeights[1];
 
@@ -196,14 +273,31 @@ export class MmdPluginMaterial extends MaterialPluginBase {
                         mat4 transformMatrix1 = mBones[int(matricesIndices[1])];
                     #endif
 
-                    vec4 slerpedRotationMatrix = slerp(
-                        rotationMatrixToQuaternion(transformMatrix0), 
-                        rotationMatrixToQuaternion(transformMatrix1),
+                    mat3 slerpedRotationMatrix = quaternionToRotationMatrix(slerp(
+                        rotationMatrixToQuaternion(mat3(transformMatrix0)), 
+                        rotationMatrixToQuaternion(mat3(transformMatrix1)),
                         weight1
-                    );
+                    ));
 
-                    // float useSdef = step(0.0, matricesSdefR0.x);
-                    // influence = mix(influence, sdefInflunce, useSdef);
+                    vec3 positionOffset = -matricesSdefC * vec3(1.0, 1.0, 1.0) +
+                        vec3(transformMatrix0 * vec4(matricesSdefR0, 1)) * weight0 +
+                        vec3(transformMatrix1 * vec4(matricesSdefR1, 1)) * weight1;
+                    
+                    mat4 sdefInflunce = mat4(slerpedRotationMatrix);
+                    sdefInflunce[3][0] = positionOffset.x;
+                    sdefInflunce[3][1] = positionOffset.y;
+                    sdefInflunce[3][2] = positionOffset.z;
+                    sdefInflunce[3][3] = 1.0;
+                        
+                    float useLinearDeform = step(0.0, -abs(matricesSdefR0.x));
+
+                    influence = mat4(
+                        mix(sdefInflunce[0], influence[0], useLinearDeform),
+                        mix(sdefInflunce[1], influence[1], useLinearDeform),
+                        mix(sdefInflunce[2], influence[2], useLinearDeform),
+                        mix(sdefInflunce[3], influence[3], useLinearDeform)
+                    );
+                }
                 #endif
                 
                 finalWorld = finalWorld * influence;
