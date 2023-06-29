518676341248becb42250ec5c62ac929e9f9c3e7
noname <48761044+noname0310@users.noreply.github.com>
2023-06-29 17:06:04 +0900

    implement bvmd converter

--- src/loader/optimized/BvmdConverter.ts
+++ src/loader/optimized/BvmdConverter.ts
@@ -33,29 +33,170 @@
  *  weights: float32[frameCount] - [..., weight, ...]
  * }[morphTrackCount]
  * 
- * propertyTrackCount: uint32
+ * propertyFrameCount: uint32
+ * frameNumbers: uint32[frameCount]
+ * visibles: uint8[frameCount] - [..., visible, ...]
+ * ikBoneNameCount: uint32
  * {
- *  trackName: uint32, uint8[] - length, string
- *  frameCount: uint32
- *  frameNumbers: uint32[frameCount]
- *  visibles: uint8[frameCount] - [..., visible, ...]
- *  ikBoneNameCount: uint32
- *  ikBoneNames: uint32[ikBoneNameCount] - uint8[] - length, string
- *  ikStates: uint8[ikBoneNameCount][frameCount] - [..., ikState, ...]
- * }[propertyTrackCount]
+ *  ikBoneName: uint32 - uint8[] - length, string
+ *  ikState: uint8[frameCount] - [..., ikState, ...]
+ * }[ikBoneNameCount]
  * 
- * cameraTrackCount: uint32
- * {
- *  trackName: uint32, uint8[] - length, string
- *  frameCount: uint32
- *  frameNumbers: uint32[frameCount]
- *  positions: float32[frameCount * 3] - [..., x, y, z, ...]
- *  positionInterpolations: uint8[frameCount * 12] - [..., x_x1, x_x2, x_y1, x_y2, y_x1, y_x2, y_y1, y_y2, z_x1, z_x2, z_y1, z_y2, ...]
- *  rotations: float32[frameCount * 3] - [..., x, y, z, ...]
- *  rotationInterpolations: uint8[frameCount * 4] - [..., x1, x2, y1, y2, ...]
- *  distances: float32[frameCount] - [..., distance, ...]
- *  distanceInterpolations: uint8[frameCount * 4] - [..., x1, x2, y1, y2, ...]
- *  fovs: float32[frameCount] - [..., fov, ...]
- *  fovInterpolations: uint8[frameCount * 4] - [..., x1, x2, y1, y2, ...]
- * }[cameraTrackCount]
+ * cameraFrameCount: uint32
+ * frameNumbers: uint32[frameCount]
+ * positions: float32[frameCount * 3] - [..., x, y, z, ...]
+ * positionInterpolations: uint8[frameCount * 12] - [..., x_x1, x_x2, x_y1, x_y2, y_x1, y_x2, y_y1, y_y2, z_x1, z_x2, z_y1, z_y2, ...]
+ * rotations: float32[frameCount * 3] - [..., x, y, z, ...]
+ * rotationInterpolations: uint8[frameCount * 4] - [..., x1, x2, y1, y2, ...]
+ * distances: float32[frameCount] - [..., distance, ...]
+ * distanceInterpolations: uint8[frameCount * 4] - [..., x1, x2, y1, y2, ...]
+ * fovs: float32[frameCount] - [..., fov, ...]
+ * fovInterpolations: uint8[frameCount * 4] - [..., x1, x2, y1, y2, ...]
  */
+
+import { MmdAnimation } from "../animation/MmdAnimation";
+import { MmdDataSerializer } from "./MmdDataSerializer";
+
+export class BvmdConverter {
+    public static Convert(animation: MmdAnimation): ArrayBuffer { 
+        const encoder = new TextEncoder();
+
+        let dataLength =
+            4 + // signature
+            3; // version
+
+        { // compute dataLength
+            dataLength += 4; // boneTrackCount
+            const boneTrackCount = animation.boneTracks.length;
+            for (let i = 0; i < boneTrackCount; i++) {
+                const boneTrack = animation.boneTracks[i];
+
+                const trackNameBytes = encoder.encode(boneTrack.name);
+                dataLength += 4 + trackNameBytes.length; // trackName
+                dataLength += 4; // frameCount
+                dataLength += 4 * boneTrack.frameNumbers.length; // frameNumbers
+                dataLength += 4 * 4 * boneTrack.frameNumbers.length; // rotations
+                dataLength += 4 * 4 * boneTrack.frameNumbers.length; // rotationInterpolations
+            }
+
+            dataLength += 4; // moveableBoneTrackCount
+            const moveableBoneTrackCount = animation.moveableBoneTracks.length;
+            for (let i = 0; i < moveableBoneTrackCount; i++) {
+                const moveableBoneTrack = animation.moveableBoneTracks[i];
+
+                const trackNameBytes = encoder.encode(moveableBoneTrack.name);
+                dataLength += 4 + trackNameBytes.length; // trackName
+                dataLength += 4; // frameCount
+                dataLength += 4 * moveableBoneTrack.frameNumbers.length; // frameNumbers
+                dataLength += 4 * 3 * moveableBoneTrack.frameNumbers.length; // positions
+                dataLength += 4 * 12 * moveableBoneTrack.frameNumbers.length; // positionInterpolations
+                dataLength += 4 * 4 * moveableBoneTrack.frameNumbers.length; // rotations
+                dataLength += 4 * 4 * moveableBoneTrack.frameNumbers.length; // rotationInterpolations
+            }
+
+            dataLength += 4; // morphTrackCount
+            const morphTrackCount = animation.morphTracks.length;
+            for (let i = 0; i < morphTrackCount; i++) {
+                const morphTrack = animation.morphTracks[i];
+                
+                const trackNameBytes = encoder.encode(morphTrack.name);
+                dataLength += 4 + trackNameBytes.length; // trackName
+                dataLength += 4; // frameCount
+                dataLength += 4 * morphTrack.frameNumbers.length; // frameNumbers
+                dataLength += 4 * morphTrack.frameNumbers.length; // weights
+            }
+
+            dataLength += 4; // property frameCount
+            dataLength += 4 * animation.propertyTrack.frameNumbers.length; // frameNumbers
+            dataLength += 1 * animation.propertyTrack.frameNumbers.length; // visibles
+            dataLength += 4; // ikBoneNameCount
+            const ikBoneNameCount = animation.propertyTrack.ikBoneNames.length;
+            for (let i = 0; i < ikBoneNameCount; i++) {
+                const ikBoneName = animation.propertyTrack.ikBoneNames[i];
+
+                const ikBoneNameBytes = encoder.encode(ikBoneName);
+                dataLength += 4 + ikBoneNameBytes.length; // ikBoneName
+            }
+            dataLength += 1 * ikBoneNameCount * animation.propertyTrack.frameNumbers.length; // ikStates
+
+            dataLength += 4; // camera frameCount
+            dataLength += 4 * animation.cameraTrack.frameNumbers.length; // frameNumbers
+            dataLength += 4 * 3 * animation.cameraTrack.frameNumbers.length; // positions
+            dataLength += 4 * 12 * animation.cameraTrack.frameNumbers.length; // positionInterpolations
+            dataLength += 4 * 3 * animation.cameraTrack.frameNumbers.length; // rotations
+            dataLength += 4 * 4 * animation.cameraTrack.frameNumbers.length; // rotationInterpolations
+            dataLength += 4 * animation.cameraTrack.frameNumbers.length; // distances
+            dataLength += 4 * 4 * animation.cameraTrack.frameNumbers.length; // distanceInterpolations
+            dataLength += 4 * animation.cameraTrack.frameNumbers.length; // fovs
+            dataLength += 4 * 4 * animation.cameraTrack.frameNumbers.length; // fovInterpolations
+        }
+
+        const data = new ArrayBuffer(dataLength);
+        const serializer = new MmdDataSerializer(data);
+
+        serializer.setUint8Array(encoder.encode("BVMD")); // signature
+        serializer.setInt8Array([1, 0, 0]); // version
+
+        const boneTracks = animation.boneTracks;
+        serializer.setUint32(boneTracks.length); // boneTrackCount
+        for (let i = 0; i < boneTracks.length; i++) {
+            const boneTrack = boneTracks[i];
+
+            serializer.setString(boneTrack.name); // trackName
+            serializer.setUint32(boneTrack.frameNumbers.length); // frameCount
+            serializer.setUint32Array(boneTrack.frameNumbers); // frameNumbers
+            serializer.setFloat32Array(boneTrack.rotations); // rotations
+            serializer.setUint8Array(boneTrack.rotationInterpolations); // rotationInterpolations
+        }
+
+        const moveableBoneTracks = animation.moveableBoneTracks;
+        serializer.setUint32(moveableBoneTracks.length); // moveableBoneTrackCount
+        for (let i = 0; i < moveableBoneTracks.length; i++) {
+            const moveableBoneTrack = moveableBoneTracks[i];
+
+            serializer.setString(moveableBoneTrack.name); // trackName
+            serializer.setUint32(moveableBoneTrack.frameNumbers.length); // frameCount
+            serializer.setUint32Array(moveableBoneTrack.frameNumbers); // frameNumbers
+            serializer.setFloat32Array(moveableBoneTrack.positions); // positions
+            serializer.setUint8Array(moveableBoneTrack.positionInterpolations); // positionInterpolations
+            serializer.setFloat32Array(moveableBoneTrack.rotations); // rotations
+            serializer.setUint8Array(moveableBoneTrack.rotationInterpolations); // rotationInterpolations
+        }
+
+        const morphTracks = animation.morphTracks;
+        serializer.setUint32(morphTracks.length); // morphTrackCount
+        for (let i = 0; i < morphTracks.length; i++) {
+            const morphTrack = morphTracks[i];
+
+            serializer.setString(morphTrack.name); // trackName
+            serializer.setUint32(morphTrack.frameNumbers.length); // frameCount
+            serializer.setUint32Array(morphTrack.frameNumbers); // frameNumbers
+            serializer.setFloat32Array(morphTrack.weights); // weights
+        }
+
+        serializer.setUint32(animation.propertyTrack.frameNumbers.length); // propertyFrameCount
+        serializer.setUint32Array(animation.propertyTrack.frameNumbers); // frameNumbers
+        serializer.setUint8Array(animation.propertyTrack.visibles); // visibles
+        serializer.setUint32(animation.propertyTrack.ikBoneNames.length); // ikBoneNameCount
+        const ikBoneNames = animation.propertyTrack.ikBoneNames;
+        for (let i = 0; i < ikBoneNames.length; i++) {
+            const ikBoneName = ikBoneNames[i];
+
+            serializer.setString(ikBoneName); // ikBoneName
+            serializer.setUint8Array(animation.propertyTrack.ikStates[i]); // ikStates
+        }
+
+        serializer.setUint32(animation.cameraTrack.frameNumbers.length); // cameraFrameCount
+        serializer.setUint32Array(animation.cameraTrack.frameNumbers); // frameNumbers
+        serializer.setFloat32Array(animation.cameraTrack.positions); // positions
+        serializer.setUint8Array(animation.cameraTrack.positionInterpolations); // positionInterpolations
+        serializer.setFloat32Array(animation.cameraTrack.rotations); // rotations
+        serializer.setUint8Array(animation.cameraTrack.rotationInterpolations); // rotationInterpolations
+        serializer.setFloat32Array(animation.cameraTrack.distances); // distances
+        serializer.setUint8Array(animation.cameraTrack.distanceInterpolations); // distanceInterpolations
+        serializer.setFloat32Array(animation.cameraTrack.fovs); // fovs
+        serializer.setUint8Array(animation.cameraTrack.fovInterpolations); // fovInterpolations
+
+        return data;
+    }
+}
---
+++ src/loader/optimized/MmdDataSerializer.ts
@@ -0,0 +1,73 @@
+export class MmdDataSerializer {
+    private static readonly _LittleEndian = true;
+
+    private readonly _dataView: DataView;
+    private _encoder: TextEncoder;
+    private _offset: number;
+
+    public constructor(arrayBuffer: ArrayBufferLike) {
+        this._dataView = new DataView(arrayBuffer);
+        this._encoder = new TextEncoder();
+        this._offset = 0;
+    }
+
+    public get offset(): number {
+        return this._offset;
+    }
+
+    public set offset(value: number) {
+        this._offset = value;
+    }
+
+    public setUint8Array(values: ArrayLike<number>): void {
+        const dataView = this._dataView;
+        for (let i = 0; i < values.length; ++i) {
+            dataView.setUint8(this._offset, values[i]);
+            this._offset += 1;
+        }
+    }
+
+    public setInt8Array(values: ArrayLike<number>): void {
+        const dataView = this._dataView;
+        for (let i = 0; i < values.length; ++i) {
+            dataView.setInt8(this._offset, values[i]);
+            this._offset += 1;
+        }
+    }
+
+    public setUint32(value: number): void {
+        this._dataView.setUint32(this._offset, value, MmdDataSerializer._LittleEndian);
+        this._offset += 4;
+    }
+
+    public setUint32Array(values: ArrayLike<number>): void {
+        const dataView = this._dataView;
+        for (let i = 0; i < values.length; ++i) {
+            dataView.setUint32(this._offset, values[i], MmdDataSerializer._LittleEndian);
+            this._offset += 4;
+        }
+    }
+
+    public setFloat32Array(values: ArrayLike<number>): void {
+        const dataView = this._dataView;
+        for (let i = 0; i < values.length; ++i) {
+            dataView.setFloat32(this._offset, values[i], MmdDataSerializer._LittleEndian);
+            this._offset += 4;
+        }
+    }
+
+    public setString(value: string): void {
+        const dataView = this._dataView;
+        const bytes = this._encoder.encode(value);
+        dataView.setUint32(this._offset, bytes.length, MmdDataSerializer._LittleEndian);
+        this._offset += 4;
+        for (let i = 0; i < bytes.length; ++i) {
+            dataView.setUint8(this._offset, bytes[i]);
+            this._offset += 1;
+        }
+    }
+
+    public get bytesAvailable(): number {
+        return this._dataView.byteLength - this._offset;
+    }
+}
