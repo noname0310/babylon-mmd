3d0e3b67988400933cd229ce4b8e8f4f522f31cb
noname <48761044+noname0310@users.noreply.github.com>
2023-07-15 06:25:44 +0900

    simplify audio player interface

--- src/runtime/audio/IAudioPlayer.ts
+++ src/runtime/audio/IAudioPlayer.ts
@@ -1,44 +1,27 @@
 import type { Observable } from "@babylonjs/core";
 
 export interface IAudioPlayer {
-    readonly onLoadErrorObservable: Observable<void>;
     readonly onDurationChangedObservable: Observable<void>;
     readonly onPlaybackRateChangedObservable: Observable<void>;
+    readonly onMuteStateChangedObservable: Observable<void>;
 
     readonly onPlayObservable: Observable<void>;
     readonly onPauseObservable: Observable<void>;
     readonly onSeekObservable: Observable<void>;
 
-    get duration(): number;
+    readonly duration: number;
 
-    get currentTime(): number;
-    set currentTime(value: number);
+    currentTime: number;
     /** @internal */
     _setCurrentTimeWithoutNotify(value: number): void;
 
-    get volume(): number;
-    set volume(value: number);
-
-    get muted(): boolean;
-    mute(): void;
-    unmute(): Promise<boolean>;
-
-    get playbackRate(): number;
-    set playbackRate(value: number);
+    playbackRate: number;
     /** @internal */
     _setPlaybackRateWithoutNotify(value: number): void;
 
-    get preservesPitch(): boolean;
-    set preservesPitch(value: boolean);
-
-    get paused(): boolean;
-
-    get source(): string;
-    set source(value: string);
-
-    get metadataLoaded(): boolean;
+    readonly paused: boolean;
+    readonly metadataLoaded: boolean;
 
     play(): Promise<void>;
-
     pause(): void;
 }
