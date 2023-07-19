80e3ce73b7c7765141bc379259bf408b6b24260a
noname <48761044+noname0310@users.noreply.github.com>
2023-07-19 19:58:52 +0900

    add publish script

---
+++ publishScript.ts
@@ -0,0 +1,20 @@
+import { execSync } from "child_process";
+import * as fs from "fs";
+
+if (fs.existsSync("./dist")) {
+    fs.rmdirSync("./dist", { recursive: true });
+}
+
+execSync("npm run build:lib");
+if (!fs.existsSync("./dist")) {
+    console.log("build failed");
+    process.exit(1);
+}
+
+fs.copyFileSync("./package.json", "./dist/package.json");
+fs.copyFileSync("./README.md", "./dist/README.md");
+fs.copyFileSync("./LICENSE", "./dist/LICENSE");
+// fs.copyFileSync("./CHANGELOG.md", "./dist/CHANGELOG.md");
+
+// execSync("npm publish ./dist");
+// fs.rmdirSync("./dist", { recursive: true });
