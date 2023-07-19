8a276715219f015e6cdf42b342b41508461dd571
noname <48761044+noname0310@users.noreply.github.com>
2023-07-19 20:08:56 +0900

    fix typo

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
-// Loader/animation
+// Loader/Animation
 export { MmdAnimation } from "@/Loader/Animation/mmdAnimation";
 export { MmdAnimationTrack, MmdBoneAnimationTrack, MmdCameraAnimationTrack, MmdMorphAnimationTrack, MmdPropertyAnimationTrack } from "@/Loader/Animation/mmdAnimationTrack";
 
-// Loader/Optimized/parser
+// Loader/Optimized/Parser
 export { BpmxObject } from "@/Loader/Optimized/Parser/bpmxObject";
 export { BpmxReader } from "@/Loader/Optimized/Parser/bpmxReader";
 
-// Loader/optimized
+// Loader/Optimized
 export { BpmxConverter } from "@/Loader/Optimized/bpmxConverter";
 export { BpmxLoader } from "@/Loader/Optimized/bpmxLoader";
 export { BvmdConverter } from "@/Loader/Optimized/bvmdConverter";
 export { BvmdLoader } from "@/Loader/Optimized/bvmdLoader";
 
-// Loader/parser
+// Loader/Parser
 export { ConsoleLogger, ILogger } from "@/Loader/Parser/ILogger";
 export { PmxObject } from "@/Loader/Parser/pmxObject";
 export { PmxReader } from "@/Loader/Parser/pmxReader";
 export { VmdData, VmdObject } from "@/Loader/Parser/vmdObject";
 
-// loader
+// Loader
 export { IMmdMaterialBuilder, MaterialInfo } from "@/Loader/IMmdMaterialBuilder";
 export { MmdAsyncTextureLoader, MmdTextureLoadResult } from "@/Loader/mmdAsyncTextureLoader";
 export { MmdModelMetadata } from "@/Loader/mmdModelMetadata";
@@ -31,17 +31,17 @@ export { SharedToonTextures } from "@/Loader/sharedToonTextures";
 export { TextureAlphaChecker, TransparencyMode } from "@/Loader/textureAlphaChecker";
 export { VmdLoader } from "@/Loader/vmdLoader";
 
-// Runtime/animation
+// Runtime/Animation
 export { MmdRuntimeAnimation, MmdRuntimeCameraAnimation, MmdRuntimeModelAnimation } from "@/Runtime/Animation/mmdRuntimeAnimation";
 
-// Runtime/audio
+// Runtime/Audio
 export { IPlayer } from "@/Runtime/Audio/IAudioPlayer";
 export { StreamAudioPlayer } from "@/Runtime/Audio/streamAudioPlayer";
 
-// Runtime/util
+// Runtime/Util
 export { DisplayTimeFormat, MmdPlayerControl } from "@/Runtime/Util/mmdPlayerControl";
 
-// runtime
+// Runtime
 export { IMmdMaterialProxy, IMmdMaterialProxyConstructor } from "@/Runtime/IMmdMaterialProxy";
 export { MmdCamera } from "@/Runtime/mmdCamera";
 export { MmdMesh, MmdMultiMaterial, RuntimeMmdMesh, RuntimeMmdModelMetadata } from "@/Runtime/mmdMesh";
