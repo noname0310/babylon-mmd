90451bf222ae11be220c9ee6de2f3ce4ae93d441
noname <48761044+noname0310@users.noreply.github.com>
2023-08-08 19:08:58 +0900

    use type import

--- src/Test/Scene/physicsTestScene.ts
+++ src/Test/Scene/physicsTestScene.ts
@@ -44,7 +44,8 @@ import { MmdCamera } from "@/Runtime/mmdCamera";
 import { MmdPhysics } from "@/Runtime/mmdPhysics";
 import { MmdRuntime } from "@/Runtime/mmdRuntime";
 import { MmdPlayerControl } from "@/Runtime/Util/mmdPlayerControl";
-import { ISceneBuilder } from "../baseRuntime";
+
+import type { ISceneBuilder } from "../baseRuntime";
 
 export class SceneBuilder implements ISceneBuilder {
     public async build(canvas: HTMLCanvasElement, engine: Engine): Promise<Scene> {
