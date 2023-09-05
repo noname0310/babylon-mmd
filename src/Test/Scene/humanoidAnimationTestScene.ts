1f7f4fb4930776522c7a55445346a8acd357360a
noname <48761044+noname0310@users.noreply.github.com>
2023-09-06 05:19:19 +0900

    create humanoid animation test

---
+++ src/Test/Scene/humanoidAnimationTestScene.ts
@@ -0,0 +1,333 @@
+import "@babylonjs/core/Loading/loadingScreen";
+import "@babylonjs/core/Lights/Shadows/shadowGeneratorSceneComponent";
+import "@babylonjs/core/Meshes/thinInstanceMesh";
+import "@babylonjs/core/Rendering/prePassRendererSceneComponent";
+import "@babylonjs/core/Rendering/depthRendererSceneComponent";
+import "@babylonjs/core/Rendering/geometryBufferRendererSceneComponent";
+import "@/Loader/Optimized/bpmxLoader";
+import "@/Runtime/Animation/mmdRuntimeCameraAnimation";
+import "@/Runtime/Animation/mmdRuntimeModelAnimation";
+
+import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
+// import { DirectionalLightFrustumViewer } from "@babylonjs/core/Debug/directionalLightFrustumViewer";
+import { SkeletonViewer } from "@babylonjs/core/Debug/skeletonViewer";
+import { Constants } from "@babylonjs/core/Engines/constants";
+import type { Engine } from "@babylonjs/core/Engines/engine";
+import { DirectionalLight } from "@babylonjs/core/Lights/directionalLight";
+import { HemisphericLight } from "@babylonjs/core/Lights/hemisphericLight";
+import { ShadowGenerator } from "@babylonjs/core/Lights/Shadows/shadowGenerator";
+import { SceneLoader } from "@babylonjs/core/Loading/sceneLoader";
+import { ImageProcessingConfiguration } from "@babylonjs/core/Materials/imageProcessingConfiguration";
+import { Material } from "@babylonjs/core/Materials/material";
+import { StandardMaterial } from "@babylonjs/core/Materials/standardMaterial";
+import { Color3, Color4 } from "@babylonjs/core/Maths/math.color";
+import { Matrix, Vector3 } from "@babylonjs/core/Maths/math.vector";
+import { CreateGround } from "@babylonjs/core/Meshes/Builders/groundBuilder";
+import type { Mesh } from "@babylonjs/core/Meshes/mesh";
+import { TransformNode } from "@babylonjs/core/Meshes/transformNode";
+import { DepthOfFieldEffectBlurLevel } from "@babylonjs/core/PostProcesses/depthOfFieldEffect";
+import { DefaultRenderingPipeline } from "@babylonjs/core/PostProcesses/RenderPipeline/Pipelines/defaultRenderingPipeline";
+import { SSRRenderingPipeline } from "@babylonjs/core/PostProcesses/RenderPipeline/Pipelines/ssrRenderingPipeline";
+import { Scene } from "@babylonjs/core/scene";
+
+import type { MmdAnimation } from "@/Loader/Animation/mmdAnimation";
+import type { MmdStandardMaterialBuilder } from "@/Loader/mmdStandardMaterialBuilder";
+import type { BpmxLoader } from "@/Loader/Optimized/bpmxLoader";
+import { BvmdLoader } from "@/Loader/Optimized/bvmdLoader";
+import { SdefInjector } from "@/Loader/sdefInjector";
+import { StreamAudioPlayer } from "@/Runtime/Audio/streamAudioPlayer";
+import { MmdCamera } from "@/Runtime/mmdCamera";
+import { MmdRuntime } from "@/Runtime/mmdRuntime";
+import { MmdPlayerControl } from "@/Runtime/Util/mmdPlayerControl";
+
+import type { ISceneBuilder } from "../baseRuntime";
+
+export class SceneBuilder implements ISceneBuilder {
+    public async build(canvas: HTMLCanvasElement, engine: Engine): Promise<Scene> {
+        SdefInjector.OverrideEngineCreateEffect(engine);
+        const pmxLoader = SceneLoader.GetPluginForExtension(".bpmx") as BpmxLoader;
+        pmxLoader.loggingEnabled = true;
+        const materialBuilder = pmxLoader.materialBuilder as MmdStandardMaterialBuilder;
+        materialBuilder.alphaEvaluationResolution = 2048;
+        // materialBuilder.loadDiffuseTexture = (): void => { /* do nothing */ };
+        // materialBuilder.loadSphereTexture = (): void => { /* do nothing */ };
+        // materialBuilder.loadToonTexture = (): void => { /* do nothing */ };
+        materialBuilder.loadOutlineRenderingProperties = (): void => { /* do nothing */ };
+        materialBuilder.afterBuildSingleMaterial = (material): void => {
+            if (material.name.toLowerCase() === "body01") material.transparencyMode = Material.MATERIAL_OPAQUE;
+            if (material.name.toLowerCase() === "face02") {
+                material.transparencyMode = Material.MATERIAL_ALPHABLEND;
+                material.useAlphaFromDiffuseTexture = true;
+                material.diffuseTexture!.hasAlpha = true;
+            }
+            if (material.name.toLowerCase() === "hairshadow") {
+                material.transparencyMode = Material.MATERIAL_ALPHABLEND;
+                material.alphaMode = Constants.ALPHA_SUBTRACT;
+                material.useAlphaFromDiffuseTexture = true;
+                material.diffuseTexture!.hasAlpha = true;
+            }
+            if (material.name.toLowerCase() === "t_floor.bmp") {
+                material.specularColor = new Color3(1, 1, 1);
+                material.specularPower = 10;
+            }
+            if (material.name.toLowerCase() === "lace") {
+                material.transparencyMode = Material.MATERIAL_ALPHATEST;
+            }
+            if (material.name.toLowerCase() === "socks") {
+                material.transparencyMode = Material.MATERIAL_OPAQUE;
+            }
+            if (material.name === "ガラス破片") {
+                material.transparencyMode = Material.MATERIAL_ALPHATESTANDBLEND;
+                material.alphaCutOff = 0;
+            }
+
+            material.useLogarithmicDepth = true;
+        };
+        pmxLoader.boundingBoxMargin = 60;
+
+        const scene = new Scene(engine);
+        scene.clearColor = new Color4(0.95, 0.95, 0.95, 1.0);
+
+        const mmdCamera = new MmdCamera("mmdCamera", new Vector3(0, 10, 0), scene);
+        mmdCamera.maxZ = 5000;
+
+        const mmdRoot = new TransformNode("mmdRoot", scene);
+        mmdCamera.parent = mmdRoot;
+        mmdRoot.position.z -= 0;
+
+        const camera = new ArcRotateCamera("arcRotateCamera", 0, 0, 45, new Vector3(0, 10, 0), scene);
+        camera.maxZ = 5000;
+        camera.setPosition(new Vector3(0, 10, -45));
+        camera.attachControl(canvas, false);
+        camera.inertia = 0.8;
+        camera.speed = 10;
+
+        const hemisphericLight = new HemisphericLight("hemisphericLight", new Vector3(0, 1, 0), scene);
+        hemisphericLight.intensity = 0.4;
+        hemisphericLight.specular = new Color3(0, 0, 0);
+        hemisphericLight.groundColor = new Color3(1, 1, 1);
+
+        const directionalLight = new DirectionalLight("directionalLight", new Vector3(0.5, -1, 1), scene);
+        directionalLight.intensity = 0.8;
+        directionalLight.autoCalcShadowZBounds = false;
+        directionalLight.autoUpdateExtends = false;
+        directionalLight.shadowMaxZ = 20;
+        directionalLight.shadowMinZ = -20;
+        directionalLight.orthoTop = 18;
+        directionalLight.orthoBottom = -3;
+        directionalLight.orthoLeft = -10;
+        directionalLight.orthoRight = 10;
+        directionalLight.shadowOrthoScale = 0;
+
+        // const directionalLightFrustumViewer = new DirectionalLightFrustumViewer(directionalLight, mmdCamera);
+        // scene.onBeforeRenderObservable.add(() => directionalLightFrustumViewer.update());
+
+        const shadowGenerator = new ShadowGenerator(1024, directionalLight, true);
+        shadowGenerator.usePercentageCloserFiltering = true;
+        shadowGenerator.forceBackFacesOnly = false;
+        shadowGenerator.bias = 0.01;
+        shadowGenerator.filteringQuality = ShadowGenerator.QUALITY_MEDIUM;
+        shadowGenerator.frustumEdgeFalloff = 0.1;
+
+        const ground = CreateGround("ground1", { width: 120, height: 120, subdivisions: 2, updatable: false }, scene);
+        const groundMaterial = ground.material = new StandardMaterial("groundMaterial", scene);
+        groundMaterial.useLogarithmicDepth = true;
+        groundMaterial.diffuseColor = new Color3(1.02, 1.02, 1.02);
+
+        const mmdRuntime = new MmdRuntime();
+        mmdRuntime.loggingEnabled = true;
+
+        mmdRuntime.register(scene);
+        mmdRuntime.playAnimation();
+
+        const audioPlayer = new StreamAudioPlayer(scene);
+        audioPlayer.preservesPitch = false;
+        audioPlayer.source = "res/private_test/motion/patchwork_staccato/pv_912.mp3";
+        mmdRuntime.setAudioPlayer(audioPlayer);
+
+        const mmdPlayerControl = new MmdPlayerControl(scene, mmdRuntime, audioPlayer);
+        mmdPlayerControl.showPlayerControl();
+
+        engine.displayLoadingUI();
+
+        let loadingTexts: string[] = [];
+        const updateLoadingText = (updateIndex: number, text: string): void => {
+            loadingTexts[updateIndex] = text;
+            engine.loadingUIText = "<br/><br/><br/><br/>" + loadingTexts.join("<br/><br/>");
+        };
+
+        const promises: Promise<any>[] = [];
+
+        const bvmdLoader = new BvmdLoader(scene);
+        bvmdLoader.loggingEnabled = true;
+
+        promises.push(bvmdLoader.loadAsync("motion", "res/private_test/motion/patchwork_staccato/motion.bvmd",
+            (event) => updateLoadingText(0, `Loading motion... ${event.loaded}/${event.total} (${Math.floor(event.loaded * 100 / event.total)}%)`))
+        );
+
+        promises.push(SceneLoader.ImportMeshAsync(
+            undefined,
+            "res/private_test/model/",
+            "YYB Hatsune Miku_10th.bpmx",
+            scene,
+            (event) => updateLoadingText(1, `Loading model... ${event.loaded}/${event.total} (${Math.floor(event.loaded * 100 / event.total)}%)`)
+        ));
+
+        pmxLoader.buildSkeleton = false;
+        pmxLoader.buildMorph = false;
+        promises.push(SceneLoader.ImportMeshAsync(
+            undefined,
+            "res/private_test/stage/",
+            "Stage35_02.bpmx",
+            scene,
+            (event) => updateLoadingText(2, `Loading stage... ${event.loaded}/${event.total} (${Math.floor(event.loaded * 100 / event.total)}%)`)
+        ));
+
+        loadingTexts = new Array(promises.length).fill("");
+
+        const loadResults = await Promise.all(promises);
+
+        mmdRuntime.setManualAnimationDuration((loadResults[0] as MmdAnimation).endFrame);
+
+        scene.onAfterRenderObservable.addOnce(() => engine.hideLoadingUI());
+
+        scene.meshes.forEach((mesh) => {
+            if (mesh.name === "skyBox") return;
+            mesh.receiveShadows = true;
+            shadowGenerator.addShadowCaster(mesh);
+        });
+
+        mmdRuntime.setCamera(mmdCamera);
+        mmdCamera.addAnimation(loadResults[0] as MmdAnimation);
+        mmdCamera.setAnimation("motion");
+
+        {
+            const modelMesh = loadResults[1].meshes[0] as Mesh;
+            modelMesh.parent = mmdRoot;
+
+            const mmdModel = mmdRuntime.createMmdModel(modelMesh, {
+                buildPhysics: true
+            });
+            mmdModel.addAnimation(loadResults[0] as MmdAnimation);
+            mmdModel.setAnimation("motion");
+
+            const bodyBone = modelMesh.skeleton!.bones.find((bone) => bone.name === "センター");
+            const meshWorldMatrix = modelMesh.getWorldMatrix();
+            const boneWorldMatrix = new Matrix();
+            scene.onBeforeRenderObservable.add(() => {
+                boneWorldMatrix.copyFrom(bodyBone!.getFinalMatrix()).multiplyToRef(meshWorldMatrix, boneWorldMatrix);
+                boneWorldMatrix.getTranslationToRef(directionalLight.position);
+                directionalLight.position.y -= 10;
+
+                camera.target.copyFrom(directionalLight.position);
+                camera.target.y += 13;
+            });
+
+            const viewer = new SkeletonViewer(modelMesh.skeleton!, modelMesh, scene, false, 3, {
+                displayMode: SkeletonViewer.DISPLAY_SPHERE_AND_SPURS
+            });
+            viewer.isEnabled = false;
+        }
+
+        const mmdStageMesh = loadResults[2].meshes[0] as Mesh;
+        mmdStageMesh.receiveShadows = true;
+        mmdStageMesh.position.y += 0.01;
+
+        const useHavyPostProcess = true;
+        const useBasicPostProcess = true;
+
+        if (useHavyPostProcess) {
+            const ssr = new SSRRenderingPipeline(
+                "ssr",
+                scene,
+                [mmdCamera, camera],
+                false,
+                Constants.TEXTURETYPE_UNSIGNED_BYTE
+            );
+            ssr.step = 32;
+            ssr.maxSteps = 128;
+            ssr.maxDistance = 500;
+            ssr.enableSmoothReflections = false;
+            ssr.enableAutomaticThicknessComputation = false;
+            ssr.blurDownsample = 2;
+            ssr.ssrDownsample = 2;
+            ssr.thickness = 0.1;
+            ssr.selfCollisionNumSkip = 2;
+            ssr.blurDispersionStrength = 0;
+            ssr.roughnessFactor = 0.1;
+            ssr.reflectivityThreshold = 0.9;
+            ssr.samples = 4;
+        }
+
+        if (useBasicPostProcess) {
+            const defaultPipeline = new DefaultRenderingPipeline("default", true, scene, [mmdCamera, camera]);
+            defaultPipeline.samples = 4;
+            defaultPipeline.bloomEnabled = true;
+            defaultPipeline.chromaticAberrationEnabled = true;
+            defaultPipeline.chromaticAberration.aberrationAmount = 1;
+            defaultPipeline.depthOfFieldEnabled = true;
+            defaultPipeline.depthOfFieldBlurLevel = DepthOfFieldEffectBlurLevel.High;
+            defaultPipeline.fxaaEnabled = true;
+            defaultPipeline.imageProcessingEnabled = true;
+            defaultPipeline.imageProcessing.toneMappingEnabled = true;
+            defaultPipeline.imageProcessing.toneMappingType = ImageProcessingConfiguration.TONEMAPPING_ACES;
+            defaultPipeline.imageProcessing.vignetteWeight = 0.5;
+            defaultPipeline.imageProcessing.vignetteStretch = 0.5;
+            defaultPipeline.imageProcessing.vignetteColor = new Color4(0, 0, 0, 0);
+            defaultPipeline.imageProcessing.vignetteEnabled = true;
+
+            defaultPipeline.depthOfField.fStop = 0.05;
+            defaultPipeline.depthOfField.focalLength = 20;
+
+            // note: this dof distance compute will broken when camera and mesh is not in same space
+
+            const modelMesh = loadResults[1].meshes[0] as Mesh;
+            const headBone = modelMesh.skeleton!.bones.find((bone) => bone.name === "頭");
+
+            const rotationMatrix = new Matrix();
+            const cameraNormal = new Vector3();
+            const cameraEyePosition = new Vector3();
+            const headRelativePosition = new Vector3();
+
+            scene.onBeforeRenderObservable.add(() => {
+                const cameraRotation = mmdCamera.rotation;
+                Matrix.RotationYawPitchRollToRef(-cameraRotation.y, -cameraRotation.x, -cameraRotation.z, rotationMatrix);
+
+                Vector3.TransformNormalFromFloatsToRef(0, 0, 1, rotationMatrix, cameraNormal);
+
+                mmdCamera.position.addToRef(
+                    Vector3.TransformCoordinatesFromFloatsToRef(0, 0, mmdCamera.distance, rotationMatrix, cameraEyePosition),
+                    cameraEyePosition
+                );
+
+                headBone!.getFinalMatrix().getTranslationToRef(headRelativePosition)
+                    .subtractToRef(cameraEyePosition, headRelativePosition);
+
+                defaultPipeline.depthOfField.focusDistance = (Vector3.Dot(headRelativePosition, cameraNormal) / Vector3.Dot(cameraNormal, cameraNormal)) * 1000;
+            });
+
+            let lastClickTime = -Infinity;
+            canvas.onclick = (): void => {
+                const currentTime = performance.now();
+                if (500 < currentTime - lastClickTime) {
+                    lastClickTime = currentTime;
+                    return;
+                }
+
+                lastClickTime = -Infinity;
+
+                if (scene.activeCamera === mmdCamera) {
+                    defaultPipeline.depthOfFieldEnabled = false;
+                    scene.activeCamera = camera;
+                } else {
+                    defaultPipeline.depthOfFieldEnabled = true;
+                    scene.activeCamera = mmdCamera;
+                }
+            };
+        }
+
+        // Inspector.Show(scene, { });
+
+        return scene;
+    }
+}
