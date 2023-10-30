e3d067e52068e3c7ad40cf09390cc727a994b1ee
noname <48761044+noname0310@users.noreply.github.com>
2023-10-30 12:39:48 +0900

    apply world scale on camera frustum and control

--- src/Test/Util/createDefaultArcRotateCamera.ts
+++ src/Test/Util/createDefaultArcRotateCamera.ts
@@ -10,11 +10,12 @@ export function createDefaultArcRotateCamera(scene: Scene, options: ICreateDefau
     const worldScale = options.worldScale ?? 1;
 
     const camera = new ArcRotateCamera("arcRotateCamera", 0, 0, 45 * worldScale, new Vector3(0, 10 * worldScale, 0), scene);
-    camera.maxZ = 5000;
+    camera.minZ = 1 * worldScale;
+    camera.maxZ = 5000 * worldScale;
     camera.setPosition(new Vector3(0, 10 * worldScale, -45 * worldScale));
     camera.attachControl(undefined, false);
     camera.inertia = 0.8;
-    camera.speed = 10;
+    camera.speed = 10 * worldScale;
 
     return camera;
 }
