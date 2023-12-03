eb113e0403ef54583f3b8918e4c7b9756dbaf0c9
noname <48761044+noname0310@users.noreply.github.com>
2023-12-03 22:05:41 +0900

    mmd runtime bindgen

--- src/Runtime/Optimized/wasm_src/src/mmd_runtime.rs
+++ src/Runtime/Optimized/wasm_src/src/mmd_runtime.rs
@@ -1,16 +1,22 @@
+use wasm_bindgen::prelude::*;
+
 use crate::mmd_model::MmdModel;
 
+#[wasm_bindgen]
 pub(crate) struct MmdRuntime {
     mmd_models: Vec<MmdModel>,
 }
 
+#[wasm_bindgen]
 impl MmdRuntime {
+    #[wasm_bindgen(constructor)]
     pub fn new() -> Self {
         MmdRuntime {
             mmd_models: Vec::new(),
         }
     }
 
+    #[wasm_bindgen(js_name = "addMmdModel")]
     pub fn add_mmd_model(&mut self) {
         
     }
