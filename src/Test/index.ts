ec3a18e35d1b2c36c8772ee07818517a80f8ea8b
noname <48761044+noname0310@users.noreply.github.com>
2023-12-08 02:39:53 +0900

    refactor: simplify test init

--- src/Test/index.ts
+++ src/Test/index.ts
@@ -1,36 +1,42 @@
 import { Engine } from "@babylonjs/core/Engines/engine";
 
+// import "@babylonjs/core/Engines/WebGPU/Extensions/engine.alpha";
+// import "@babylonjs/core/Engines/WebGPU/Extensions/engine.rawTexture";
+// import "@babylonjs/core/Engines/WebGPU/Extensions/engine.renderTarget";
+// import "@babylonjs/core/Engines/WebGPU/Extensions/engine.uniformBuffer";
+// import { WebGPUEngine } from "@babylonjs/core/Engines/webgpuEngine";
 import { BaseRuntime } from "./baseRuntime";
-import { SceneBuilder } from "./Scene/physicsTestScene";
+import { SceneBuilder } from "./Scene/bakedAnimationTestScene";
 
-window.onload = (): void => {
-    const canvas = document.createElement("canvas");
-    canvas.style.width = "100%";
-    canvas.style.height = "100%";
-    canvas.style.display = "block";
-    document.body.appendChild(canvas);
+await new Promise(resolve => window.onload = resolve);
 
-    const engine = new Engine(canvas, false, {
-        preserveDrawingBuffer: false,
-        stencil: false,
-        antialias: false,
-        alpha: false,
-        premultipliedAlpha: false,
-        powerPreference: "high-performance",
-        doNotHandleTouchAction: true,
-        doNotHandleContextLost: true,
-        audioEngine: false
-    }, true);
+const canvas = document.createElement("canvas");
+canvas.style.width = "100%";
+canvas.style.height = "100%";
+canvas.style.display = "block";
+document.body.appendChild(canvas);
 
-    // const webGPUEngine = new WebGPUEngine(canvas, {
-    //     stencil: true,
-    //     antialias: true
-    // });
-    // await webGPUEngine.initAsync();
+const engine = new Engine(canvas, false, {
+    preserveDrawingBuffer: false,
+    stencil: false,
+    antialias: false,
+    alpha: false,
+    premultipliedAlpha: false,
+    powerPreference: "high-performance",
+    doNotHandleTouchAction: true,
+    doNotHandleContextLost: true,
+    audioEngine: false,
+    disableWebGL2Support: false
+}, true);
 
-    BaseRuntime.Create({
-        canvas,
-        engine,
-        sceneBuilder: new SceneBuilder()
-    }).then(runtime => runtime.run());
-};
+// const engine = new WebGPUEngine(canvas, {
+//     stencil: true,
+//     antialias: true
+// });
+// await engine.initAsync();
+
+BaseRuntime.Create({
+    canvas,
+    engine,
+    sceneBuilder: new SceneBuilder()
+}).then(runtime => runtime.run());
