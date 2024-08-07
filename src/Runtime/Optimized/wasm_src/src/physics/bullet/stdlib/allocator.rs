d70af34cdf46ac89547210555099bdbb77b43cdc
noname <48761044+noname0310@users.noreply.github.com>
2024-08-07 22:05:15 +0900

    tabs to space lint

--- src/Runtime/Optimized/wasm_src/src/physics/bullet/stdlib/allocator.rs
+++ src/Runtime/Optimized/wasm_src/src/physics/bullet/stdlib/allocator.rs
@@ -14,25 +14,25 @@ const MAX_ALIGN: usize = 16;
 /// See [malloc](https://linux.die.net/man/3/malloc) for alignment details.
 #[no_mangle]
 unsafe extern "C" fn bw_malloc(size: CSizeT) -> *mut u8 {
-	// size + MAX_ALIGN for to store the size of the allocated memory.
-	let layout = alloc::alloc::Layout::from_size_align(size + MAX_ALIGN, MAX_ALIGN).unwrap();
-	let ptr = unsafe { alloc::alloc::alloc(layout) };
-	if ptr.is_null() {
-		return ptr;
-	}
-	unsafe {
-		*(ptr as *mut CSizeT) = size;
-	}
-	ptr.add(MAX_ALIGN)
+    // size + MAX_ALIGN for to store the size of the allocated memory.
+    let layout = alloc::alloc::Layout::from_size_align(size + MAX_ALIGN, MAX_ALIGN).unwrap();
+    let ptr = unsafe { alloc::alloc::alloc(layout) };
+    if ptr.is_null() {
+        return ptr;
+    }
+    unsafe {
+        *(ptr as *mut CSizeT) = size;
+    }
+    ptr.add(MAX_ALIGN)
 }
 
 /// Rust implementation of C library function `free`
 #[no_mangle]
 unsafe extern "C" fn bw_free(ptr: *mut u8) {
-	if ptr.is_null() {
-		return;
-	}
-	let old_size = unsafe { *(ptr.sub(MAX_ALIGN) as *mut CSizeT) };
-	let layout = alloc::alloc::Layout::from_size_align(old_size + MAX_ALIGN, MAX_ALIGN).unwrap();
-	unsafe { alloc::alloc::dealloc(ptr.sub(MAX_ALIGN), layout) };
+    if ptr.is_null() {
+        return;
+    }
+    let old_size = unsafe { *(ptr.sub(MAX_ALIGN) as *mut CSizeT) };
+    let layout = alloc::alloc::Layout::from_size_align(old_size + MAX_ALIGN, MAX_ALIGN).unwrap();
+    unsafe { alloc::alloc::dealloc(ptr.sub(MAX_ALIGN), layout) };
 }
