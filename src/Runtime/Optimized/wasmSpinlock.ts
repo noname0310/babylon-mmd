f2eb6307a658638d06324719499f4c3551bf43ef
noname0310 <hjnam2014@gmail.com>
2025-01-20 08:42:32 +0900

    revert spinlock timeout

--- src/Runtime/Optimized/wasmSpinlock.ts
+++ src/Runtime/Optimized/wasmSpinlock.ts
@@ -1,8 +1,8 @@
 import type { IWasmTypedArray } from "./IWasmTypedArray";
 
-const enum SpinLockConstants {
-    Timeout = 10000, // 10 seconds
-}
+// const enum SpinLockConstants {
+//     Timeout = 10000, // 10 seconds
+// }
 
 /**
  * Spinlock for WASM runtime synchronization
@@ -24,13 +24,12 @@ export class WasmSpinlock {
     public wait(): void {
         const lock = this._lock.array;
         // let locked = false;
-        const lockStartTime = performance.now();
+        // const lockStartTime = performance.now();
         while (Atomics.load(lock, 0) !== 0) {
-            if (performance.now() - lockStartTime > SpinLockConstants.Timeout) {
-                throw new Error("Spinlock timeout");
-            }
+            // if (performance.now() - lockStartTime > SpinLockConstants.Timeout) {
+            //     throw new Error("Spinlock timeout");
+            // }
             // locked = true;
-            // spin
         }
         // if (locked) {
         //     const lockTime = performance.now() - lockStartTime;
