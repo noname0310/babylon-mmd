d2e6e6ff031437a82ca9802eac5c9274d3f98a4d
noname0310 <hjnam2014@gmail.com>
2025-02-25 00:27:17 +0900

    add missing jsdoc

--- src/Loader/registerDxBmpTextureLoader.ts
+++ src/Loader/registerDxBmpTextureLoader.ts
@@ -2,6 +2,13 @@ import { registerTextureLoader } from "@babylonjs/core/Materials/Textures/Loader
 
 let registered = false;
 
+/**
+ * Register the bmp texture loader that behaves like the directx bmp texture loader
+ *
+ * This is completely optional and is required to accurately load MMD models that contain BMP textures with alpha channels.
+ *
+ * if the loader is already registered, this function does nothing
+ */
 export function registerDxBmpTextureLoader(): void {
     if (registered) {
         return;
