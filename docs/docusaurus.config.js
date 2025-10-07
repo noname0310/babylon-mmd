b2188d2055a970a3f29eeb429660406fdfe7fcdf
noname0310 <hjnam2014@gmail.com>
2025-10-08 04:22:47 +0900

    Update Docusaurus config and dependencies

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -22,7 +22,9 @@ const config = {
     projectName: "babylon-mmd", // Usually your repo name.
 
     onBrokenLinks: "throw",
-    onBrokenMarkdownLinks: "warn",
+    markdown: {
+        hooks: { onBrokenMarkdownLinks: "warn" }
+    },
 
     // Even if you don"t use internalization, you can use this field to set useful
     // metadata like html lang. For example, if your site is Chinese, you may want
